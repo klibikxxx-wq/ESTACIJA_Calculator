4ee5dcc0580aaf434e92862379568220d36a0791
klibikxxx-wq <klibikxxx@gmail.com>
2026-02-26 18:47:37 +0200

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ with st.form("ievades_forma"):
     with col_input1:
         usage = st.number_input("Mēneša patēriņš (kWh)", min_value=1, value=9000)
     with col_input2:
-        bill = st.number_input("Mēneša rēķins (€ ar PVN)", min_value=1.0, value=1500.0)
         bill = st.number_input("Mēneša rēķins (€ ar PVN)", min_value=1.0, value=250.0)
     
     submit_button = st.form_submit_button("Aprēķināt risinājumu")
